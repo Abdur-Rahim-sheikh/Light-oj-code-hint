8b8b8282712afddc42d6d73d32fcf3624491e50a
Abdur-Rahim-sheikh <39773406+Abdur-Rahim-sheikh@users.noreply.github.com>
2019-11-23 01:14:14 +0600

    Create .c

---
+++ Begineer/1001 opposite task/.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+int main()
+{
+     int i,j,k,m,n,rem,div,val,previous,test;
+    scanf("%lld",&test);
+    while(test--)
+    {
+        scanf("%d",&n);
+        if(n>10){rem=n-10;n=10;}
+        else {rem=0;}
+        printf("%d %d\n",rem,n);
+    }
+
+
+    return 0;
+}
