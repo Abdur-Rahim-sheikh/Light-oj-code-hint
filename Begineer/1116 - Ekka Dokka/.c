ac2ee89d09865b8af7c5adc4b255e8a161cc9bb5
Abdur-Rahim-sheikh <39773406+Abdur-Rahim-sheikh@users.noreply.github.com>
2019-11-24 18:16:25 +0600

    Update .c

--- Begineer/1116 - Ekka Dokka/.c
+++ Begineer/1116 - Ekka Dokka/.c
@@ -10,29 +10,15 @@ int main()
     for(j=0;j<n;j++)
     {
         scanf("%lld",&val);
-        if(val%2==1) printf("Case %lld: Impossible\n",j+1);
+        if(val&1) printf("Case %lld: Impossible\n",j+1);
         else{
-            val=val/2;
-            for(i=0,sum=1,sign=0;sum<val;i++)
-            {
+            sum=1;
+            while(val%2!=1){
+                val=val/2;
                 sum=sum*2;
-                if(sum==val){
-                     sign=1;
-                      break;
-                }
             }
-            if(sign==1) printf("Case %lld: Impossible\n",j+1);
-            else{
-                sum=2;
-                while(val%2!=1)
-                {
-                    val=val/2;
-                    sum=sum*2;
-                }
-                printf("Case %lld: %lld %lld\n",j+1,val,sum);
-            }
+            printf("Case %lld: %lld %lld\n",j+1,val,sum);
         }
-
     }
     return 0;
 }
