5ee99d8eeba3273293683ad4826b38d8c4bf4da4
Abdur-Rahim-sheikh <39773406+Abdur-Rahim-sheikh@users.noreply.github.com>
2020-03-13 20:39:42 +0600

    Create .cpp

---
+++ Begineer/1331 - Agent J/.cpp
@@ -0,0 +1,11 @@
+## Key Idea
+
+1) r1,r2,r3 is given and edge of these circle are touched 
+if we think of a triangle through these three centre. 
+then length of three sides are
+a=r1+r2
+b=r1+r3
+c=r2+r3
+2) then shaded area is (area of triangle - circular sector of r1 -circular sector of r2 - circular sector of r3)
+3) through these three arm we can calculate their angle theta 1,2,3
+4) Using this angle we can calculate circular sector
