33e2531a4c0516da41af13578aae2d25f81d1a89
Abdur-Rahim-sheikh <39773406+Abdur-Rahim-sheikh@users.noreply.github.com>
2020-08-20 23:29:17 +0600

    Create .cpp

---
+++ Basic Math/1179 - Josephus Problem/.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define INF 1073741824
+#define ll long long
+#define PI 2*acos(0.0)
+#define mp make_pair
+#define pb push_back
+#define pii pair<int,int>
+
+using namespace std;
+int josephus(int n,int k){//this k is the gap
+    if(n==1)return 1;
+    return (josephus(n-1,k)+k-1)%n + 1;
+}
+int main(){
+    int i,j,k,m,n,val,t=0,cnt,test;
+    cin>>test;
+
+    while(test--){
+        scanf("%d %d",&n,&k);
+        int m=k;
+        if(k==1){
+            printf("Case %d: %d\n",++t,n);
+            continue;
+        }
+        printf("Case %d: %d\n",++t,josephus(n,k-1));
+
+    }
+    //free();   //if pointer array
+    return 0;
+}
